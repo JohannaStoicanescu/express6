763058f088c6e739e1ef853a228523912cbaec59
Johanna <johanna.stoicanescu@gmail.com>
2022-11-13 18:08:40 +0100

    commit

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
 /* USERS */
 
 app.get("/api/users", userHandlers.getUsers);
@@ -40,6 +42,8 @@ app.post("/api/users", validateUser, userHandlers.postUser);
 /*app.post("/api/users", userHandlers.postUser);*/
 
 app.put("/api/users/:id", userHandlers.updateUser);
+
+app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
